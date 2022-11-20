3928efb7e85cbdd1a3703ef5bb548c8211a37133
Borges10002 <borges10002@gmail.com>
2022-11-20 10:06:43 -0400

    atualizacao

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,7 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 
 import '../styles/global.css';
 
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp
